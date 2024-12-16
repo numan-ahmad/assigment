5a7d5eff8fbb6fd6d9338a886b49c221bb7d59f2
numan-ahmad <na56439@gmail.com>
2024-12-16 12:09:37 +0500

    removed the code

--- backend/server/routes/auth-route.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { login } = require("../controller/authController");
-const router = express.Router();
-
-router.post("/login", login);
-module.exports = router;
--- backend/server/routes/vehicle-form-route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { vehicleSubmission } = require("../controller/vehicalFormController");
-const router = express.Router();
-const upload = require("../middlewares/upload");
-
-router.post("/vehicle-submission", upload.array("images"), vehicleSubmission);
-module.exports = router;
